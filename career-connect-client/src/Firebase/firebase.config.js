f55a615502155dd7c897da9e21b7cfbb4c137963
Tavish Bhardwaj <bhardwajtavishofficial@gmail.com>
2024-04-25 14:20:13 +0530

    firebase apikey removed.

--- career-connect-client/src/Firebase/firebase.config.js
+++ career-connect-client/src/Firebase/firebase.config.js
@@ -3,11 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+// require("D:/CareerConnect-FullStack/career-connect-server/.env").config();
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAnQ7ca2AtE-lMtFnhPbGNbH2DQWVH3tJw",
+  apiKey: window.env.API_KEY ,
   authDomain: "career-connect-job-portal.firebaseapp.com",
   projectId: "career-connect-job-portal",
   storageBucket: "career-connect-job-portal.appspot.com",
